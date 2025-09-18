446335a7fe1351b071d242f9abcf00d54dc32dba
caleblopez96 <caleblopez96@gmail.com>
2025-09-18 16:07:47 -0700

    finished assignment

---
+++ script.js
@@ -0,0 +1,118 @@
+// Function runs on page load to view current popular movies in the US
+// endpoint here: https://developer.themoviedb.org/reference/movie-popular-list
+function getPopularMovies() {
+    // the endpoint
+    let url = `https://api.themoviedb.org/3/movie/popular?api_key=502fe1fd9a6855cc46b5147c5e7a4629&language=en-US&page=1`;
+    let endpoint;
+    // the place on the page where we'll display the movies
+    let popularMovies = document.getElementById("popular");
+    let imgUrl = "https://image.tmdb.org/t/p/w400";
+
+    // ajax time!
+    // create the object
+    const xhr = new XMLHttpRequest();
+
+    // attach event handlers
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            let json = this.response;
+
+            let html = "";
+
+            html += `
+            <section id="featured">
+                <h3>${json.results[0].title}</h3>
+                <img src="${imgUrl}${json.results[0].poster_path}" alt="">
+                <p>"${json.results[0].overview}"</p>
+            </section>`;
+
+            for (let i = 1; i < 19; i++) {
+                html += `<section class="movie">
+                <img src="${imgUrl}${json.results[i].poster_path}" alt="">
+                <div>
+                    <h3>${json.results[i].title}</h3>
+                    <p>
+                        ${json.results[i].overview}
+                        <span class="vote">Vote Average: ${json.results[i].vote_average}</span>
+                    </p>
+                </div>
+            </section>`;
+            }
+
+            popularMovies.innerHTML = html;
+        }
+    });
+
+    // set the response type
+    xhr.responseType = "json";
+
+    // open the request
+    xhr.open("GET", url);
+
+    // send the request
+    xhr.send();
+}
+
+// function runs only after a year is entered/chosen and submitted through the form
+// endpoint here: https://developer.themoviedb.org/reference/discover-movie
+function getBirthYearMovies(e) {
+    e.preventDefault();
+
+    // Get the user's input/year value
+    let year = encodeURI(document.querySelector("#userYear").value);
+
+    // the place on the page where we'll add the movies
+    let birthYearMovies = document.getElementById("birthYear");
+
+    if (year < 1940 || year > 2024 || year == "") {
+        birthYearMovies.innerHTML = `<p style="color: red; background-color: white;">Please enter a year between 1940 and 2022</p>`;
+    } else {
+        // TO DO - Build the endpoint we need (this one has additional parameters)
+        let baseUrl =
+            "https://api.themoviedb.org/3/discover/movie?api_key=502fe1fd9a6855cc46b5147c5e7a4629&include_adult=false&include_video=false&language=en-US&page=1&primary_release_year=";
+        let endUrl = "&sort_by=revenue.desc";
+        let imgUrl = "https://image.tmdb.org/t/p/w400";
+
+        // ajax time!
+        // create the object
+        let xhr = new XMLHttpRequest();
+
+        // attach event handlers
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === this.DONE) {
+                let json = this.response;
+
+                let html = "";
+
+                let counter = 0;
+                for (let i = 0; counter < 6; i++) {
+                    if (json.results[i].poster_path === null) {
+                        continue;
+                    } else {
+                        html += `<section class="yrMovie">
+                            <img src="${imgUrl}${json.results[i].poster_path}" alt="">
+                            <h3>${json.results[i].title}</h3>
+                        </section>`;
+                        counter++;
+                    }
+                }
+                birthYearMovies.innerHTML = html;
+            }
+        });
+
+        // set the response type
+        xhr.responseType = "json";
+
+        // open the request
+        xhr.open("GET", `${baseUrl}${year}${endUrl}`);
+        // attach the headers (optional)
+
+        // send the request
+        xhr.send();
+    }
+}
+
+window.addEventListener("load", function () {
+    getPopularMovies();
+    document.getElementById("yearBtn").addEventListener("click", getBirthYearMovies);
+});
